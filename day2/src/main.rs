4d2bae2827f0f1006c58c1b964b3b83b4c367823
tg6952 <tg6952@gmail.com>
2018-12-02 11:10:23 -0700

    Day 2 - Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,7 @@
 mod util;
 
+use std::iter::Iterator;
+
 fn main() {
     let mut buffer: String = String::new();
     let box_ids: Vec<&str> =
@@ -7,6 +9,43 @@ fn main() {
 
     let checksum: u32 = get_checksum(&box_ids);
     println!("Checksum: {}", checksum);
+
+    let common_letters: String = get_common_letters(&box_ids, 1);
+    println!("Common letters: {}", common_letters);
+}
+
+fn get_common_letters(box_ids: &Vec<&str>, differing_by: u8) -> String {
+    // find the first two strings which have only one difference
+    for id_1 in box_ids.iter() {
+        for id_2 in box_ids.iter() {
+            if get_differences(id_1, id_2) == differing_by {
+                // id_1 and id_2 differ by the specified ammount
+                // build a string from the identical letters, and return
+                let mut buffer: String = String::new();
+
+                for (ch_1, ch_2) in id_1.chars().zip(id_2.chars()) {
+                    if ch_1 == ch_2 {
+                        buffer.push(ch_1);
+                    }
+                }
+
+                return buffer;
+            }
+        }
+    }
+
+    return String::new();
+}
+
+fn get_differences(id_1: &str, id_2: &str) -> u8 {
+    let mut differences: u8 = 0;
+    for (ch_1, ch_2) in id_1.chars().zip(id_2.chars()) {
+        if ch_1 != ch_2 {
+            differences += 1;
+        }
+    }
+
+    return differences;
 }
 
 fn get_checksum(box_ids: &Vec<&str>) -> u32 {
